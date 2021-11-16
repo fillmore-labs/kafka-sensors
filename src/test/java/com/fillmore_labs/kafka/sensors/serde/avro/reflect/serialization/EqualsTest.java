4ac544b377d47908c967f2bb774d9be3f61a45f7
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2021-11-16 21:31:00 +0100

    Simplify equals tests
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- src/test/java/com/fillmore_labs/kafka/sensors/serde/avro/reflect/serialization/EqualsTest.java
+++ src/test/java/com/fillmore_labs/kafka/sensors/serde/avro/reflect/serialization/EqualsTest.java
@@ -2,31 +2,26 @@ package com.fillmore_labs.kafka.sensors.serde.avro.reflect.serialization;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.util.List;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.Test;
 
 public class EqualsTest {
+  private static final Class<?>[] CLASSES = {
+    ReadingReflect.class, SensorStateReflect.class, StateDurationReflect.class
+  };
+
   @Test
   public void testEquals() {
-    EqualsVerifier.forClasses(
-            ReadingReflect.class, SensorStateReflect.class, StateDurationReflect.class)
-        .suppress(Warning.NONFINAL_FIELDS)
-        .verify();
+    EqualsVerifier.forClasses(List.of(CLASSES)).suppress(Warning.NONFINAL_FIELDS).verify();
   }
 
   @Test
-  public void testToString1() {
-    assertThat(new ReadingReflect().toString()).isNotEmpty();
-  }
-
-  @Test
-  public void testToString2() {
-    assertThat(new SensorStateReflect().toString()).isNotEmpty();
-  }
-
-  @Test
-  public void testToString3() {
-    assertThat(new StateDurationReflect().toString()).isNotEmpty();
+  public void testToString() throws ReflectiveOperationException {
+    for (var clazz : CLASSES) {
+      var t = clazz.getDeclaredConstructor().newInstance();
+      assertThat(t.toString()).isNotEmpty();
+    }
   }
 }
