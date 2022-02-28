29e19720d352ac376b7f22a12cdea953870ce20a
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2022-02-28 23:18:00 +0100

    CI messages
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- .buildkite/upload_coverage.sh
+++ .buildkite/upload_coverage.sh
@@ -2,14 +2,14 @@
 
 COVERAGE="$(bazel info output_path)/_coverage/_coverage_report.dat"
 
-echo ":codecov: Upload Coverage"
+echo "Upload Coverage"
 codecov -f "$COVERAGE" &
 PID1=$!
 
-echo ":codeclimate: Upload Coverage"
+echo "Upload Coverage"
 cc-test-reporter format-coverage -t lcov -o .coverage/codeclimate.json "$COVERAGE"
 cc-test-reporter upload-coverage -r "$CC_TEST_REPORTER_ID" -i .coverage/codeclimate.json &
 PID2=$!
 
 wait $PID1 $PID2 || true
-echo ":trollface: Coverage Upload Done"
+echo "Coverage Upload Done"
