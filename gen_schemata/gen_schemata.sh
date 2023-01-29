1f7589cd82bacb43a71ee6d278da8737958f7e59
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2023-01-29 11:09:36 +0100

    remove manual schema generate [skip ci]

--- gen_schemata/gen_schemata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR="$(dirname "$0")"
-cd "$SCRIPT_DIR" || exit
-
-./avro-tools.sh idl2schemata ./state_duration.avdl ../src/main/avro/com/fillmore_labs/kafka/sensors/avro
-
-echo "Remove duplicate definitions."
